508947ddc5c746b5aa7afa396a4527af771eef4b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-05-20 20:39:40 +0300

    Create part-z-0002-component-props1-App.jsx

---
+++ part-z-0002-component-props1-App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import Greeting from "./components/Greeting"
+
+function App() {
+
+  return (
+    <>
+
+      <Greeting firstName="Ayhan" lastName="Bilir" />
+      <Greeting firstName="Kağan" lastName="Yapıcı" />
+      <Greeting firstName="Bengü" lastName="Satıcı" />
+      
+    </>
+  )
+}
+
+export default App
