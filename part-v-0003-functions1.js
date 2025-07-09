35802687cf1be4919d60330b3ac97a7f1c45f835
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-09 23:48:44 +0300

    Create part-v-0003-functions1.js

---
+++ part-v-0003-functions1.js
@@ -0,0 +1,9 @@
+//  Your Goal: Return a string from getMessage
+// In the getMessage function return a string. It can be any string that you choose!
+
+function getMessage() {
+     
+     return "Mustafa"
+}
+
+module.exports = getMessage;
