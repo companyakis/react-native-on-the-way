ee6ce6f6b3d8ffb773af010079766c79219664d6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-09 23:41:23 +0300

    Create part-v-0002-variables-types1.js

---
+++ part-v-0002-variables-types1.js
@@ -0,0 +1,10 @@
+// // alchemy university challenges
+
+// //  Your Goal: Store Number in a
+// // Your goal on this stage is to simply give the variable a some number value.
+// // It could be any number. Perhaps it's your favorite number, maybe it's a floating point number 12.5 or perhaps it's a negative number -22! It's all up to you. 
+// You'll do this by typing a number on the right-hand side of the const a = statement.
+
+const a = 21
+
+module.exports = a;
