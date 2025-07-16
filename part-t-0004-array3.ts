0c150a0dd5acf1b5ddfb05d7a828237e9bf40cd3
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-16 13:23:10 +0300

    Create part-t-0004-array3.ts

---
+++ part-t-0004-array3.ts
@@ -0,0 +1,11 @@
+let ages: number[] = [9, 25, 87, 47, 95]
+
+let new_ages: number[] = ages.map((age) => age + 5)
+
+console.log(ages)
+
+console.log(new_ages)
+
+// [LOG]: [9, 25, 87, 47, 95] 
+
+// [LOG]: [14, 30, 92, 52, 100] 
