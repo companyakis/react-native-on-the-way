0cd3b74937ee964e8ce483252e2c7b36025b44a1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-16 12:46:25 +0300

    Delete part-v-0003-functions1.js

--- part-v-0003-functions1.js
+++
@@ -1,9 +0,0 @@
-//  Your Goal: Return a string from getMessage
-// In the getMessage function return a string. It can be any string that you choose!
-
-function getMessage() {
-     
-     return "Mustafa"
-}
-
-module.exports = getMessage;
