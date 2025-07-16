6e20d4f86d8c84b872999c868d6190c1ea1fbd02
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-16 16:56:09 +0300

    Create part-t-0005-tuple2.ts

---
+++ part-t-0005-tuple2.ts
@@ -0,0 +1,11 @@
+let quarterly_sales_data: [string, number][]
+
+quarterly_sales_data = [
+
+  ["quarter 1", 587_000],
+  ["quarter 2", 624_354],
+  ["quarter 3", 871_900],
+  ["quarter 4", 712_400],
+]
+
+console.log(`${quarterly_sales_data[2][0]} sales amount: $${quarterly_sales_data[2][1]}`) // "quarter 3 sales amount: $871900" 
