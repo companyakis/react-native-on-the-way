3b1bc910edbc65eaeba2f2fc13b13fdb1e030986
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-16 12:59:08 +0300

    Create part-t-0001-hello.ts

---
+++ part-t-0001-hello.ts
@@ -0,0 +1,14 @@
+let writer: string = "Mustafa Buyukdereli"
+
+let year: number = 2025
+
+let purpose: string = "React Native Apps with TypeScript"
+
+function print(): void {
+
+  console.log(writer)
+  console.log(year)
+  console.log(purpose)
+}
+
+print()
