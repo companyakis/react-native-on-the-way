8f86f2c7a6d9d1bfa8dd8d551cc01253b0c7a619
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-07-16 13:27:10 +0300

    Create part-t-0004-array4.ts

---
+++ part-t-0004-array4.ts
@@ -0,0 +1,7 @@
+let names: string[] = ["mustafa", "bilge", "bengu", "kultigin", "kutluk"]
+
+let new_names: string[] = names.map((n) => n.toUpperCase())
+
+console.log(new_names)
+
+// [LOG]: ["MUSTAFA", "BILGE", "BENGU", "KULTIGIN", "KUTLUK"] 
